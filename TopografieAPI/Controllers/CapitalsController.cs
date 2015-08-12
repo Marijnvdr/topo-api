a248439d29e22dbde2f22f9a5f50d2d59b17c280
Marijnvdr <mvdruijt@gmail.com>
2015-08-12 20:30:39 +0200

    First API test with capital

---
+++ TopografieAPI/Controllers/CapitalsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using TopografieAPI.Models;
+
+namespace TopografieAPI.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class CapitalsController : ApiController
+    {
+        // GET api/topo/5
+        public CapitalViewModel Get(int id)
+        {
+            if (id < 2)
+            {
+                return new CapitalViewModel { /*Id = id.ToString(),*/ Name = "Amsterdam", Continent = "Europa", Country = "Nederland" };
+            }
+            else
+                return new CapitalViewModel { /*Id = id.ToString(),*/ Name = "Parijs", Continent = "Europa", Country = "Frankrijk" };
+        }
+
+    }
+}
---
+++ TopografieAPI/Models/CapitalViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TopografieAPI.Models
+{
+    public class CapitalViewModel
+    {
+        //public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Country { get; set; }
+
+        public string Continent { get; set; }
+
+        //public int Population { get; set; }
+    }
+}
