155e27182efdd92aac73d81ad825d24fe6035f32
Marijnvdr <mvdruijt@gmail.com>
2015-08-12 15:03:05 +0200

    Initial setup

---
+++ TopografieAPI/Areas/HelpPage/ModelDescriptions/EnumValueDescription.cs
@@ -0,0 +1,11 @@
+namespace TopografieAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class EnumValueDescription
+    {
+        public string Documentation { get; set; }
+
+        public string Name { get; set; }
+
+        public string Value { get; set; }
+    }
+}
