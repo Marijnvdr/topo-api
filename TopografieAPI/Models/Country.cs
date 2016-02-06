ff488f5cc7c14e90fe925f62118dcb8932e11d6c
mvdr <m.ruijt@niposoftware.com>
2016-02-06 16:39:24 +0100

    local computername in web.config

--- TopografieAPI/Models/Country.cs
+++ TopografieAPI/Models/Country.cs
@@ -7,6 +7,8 @@ namespace TopografieAPI.Models
 {
     public class Country
     {
+        public string Id { get; set; }
+
         public string Name { get; set; }
 
         public string Name_nl { get; set; }
